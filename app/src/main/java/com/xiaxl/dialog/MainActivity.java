b02b9ef3426353d5f4318505c80de3ddb564c2d1
xiaxl <bjxiaxueliang@corp.netease.com>
2018-01-31 11:00:43 +0800

    init

---
+++ app/src/main/java/com/xiaxl/dialog/MainActivity.java
@@ -0,0 +1,16 @@
+package com.xiaxl.dialog;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
